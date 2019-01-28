c5a3761366e7796d8a2737100c8caca70844ae60
Juuso Nykänen <juusonykanen@hotmail.com>
2019-01-28 03:53:11 -0800

    refactor to use javascript object for data

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,47 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const course = 'Half Stack -sovelluskehitys';
-  const part1 = 'Reactin perusteet';
-  const exercises1 = 10;
-  const part2 = 'Tiedonvälitys propseilla';
-  const exercises2 = 7;
-  const part3 = 'Komponenttien tila';
-  const exercises3 = 14;
 
-  const data = [
-    { 
-      id: '1',
-      part: part1,
-      exercises: exercises1,
-    },
-    { 
-      id: '2',
-      part : part2,
-      exercises: exercises2,
-    },
-    { 
-      id: '3',
-      part : part3,
-      exercises: exercises3,
-    }
-  ];
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      { 
+        id: '1',
+        part: 'Reactin perusteet',
+        exercises: 10,
+      },
+      { 
+        id: '2',
+        part : 'Tiedonvälitys propseilla',
+        exercises: 7,
+      },
+      { 
+        id: '3',
+        part : 'Komponenttien tila',
+        exercises: 14,
+      }
+    ]
+  };
+
+  let sum = 0;
+  course.parts.forEach(e => sum += e.exercises);
+  course.total = sum;
 
   return (
     <div>
-      <Header course={course} />
-      <Content data={data} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.total} />
     </div>
   );
 }
 
-const Header = (props) => <h1> {props.course} </h1>;
+const Header = props => <h1> {props.name} </h1>;
 
 const Content = (props) => {
   return (
     <>
-      {props.data.map(element => <Part {...element} key={element.id} />)}
+      {props.parts.map(element => <Part {...element} key={element.id} />)}
     </>
   );
 }
