6d021b67469f11a1cce58d6e36b0ac563d4abf00
Juuso Nykänen <juusonykanen@hotmail.com>
2019-02-17 11:31:15 -0800

    work in progress on 4.2

---
+++ osa4/blogilista/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+	"no-console": 0,
+	"prefer-destructuring": 0,
+	"consistent-return": 0,
+	"no-underscore-dangle": 0,
+	"no-param-reassign": 0,
+	"arrow-parens": 0,
+    }
+};
