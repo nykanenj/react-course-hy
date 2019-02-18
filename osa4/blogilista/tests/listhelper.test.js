9d8739ad27f94cdb0bdccdac73e463364207fbd8
Juuso Nykänen <juusonykanen@hotmail.com>
2019-02-18 12:43:08 -0800

    4.6, 4.7 done

--- osa4/blogilista/tests/listhelper.test.js
+++ osa4/blogilista/tests/listhelper.test.js
@@ -1,24 +1,60 @@
 const listHelper = require('../utils/list_helper');
 
-describe('dummy tests', () => { 
-  test('dummy return 1', () => {
-    const blogs = [];
+const noblogs = [];
 
-    const result = listHelper.dummy(blogs);
+const blogs = [
+  {
+    title: 'Kesäkurpitsa pizza reseptit',
+    author: 'Heli',
+    url: 'www.kurpizzab.blog',
+    likes: 5,
+    id: '5c69a6d19cb54625f7aa4246',
+  },
+  {
+    title: 'Jonnen Jälkiruoat',
+    author: 'Jonne Jononen',
+    url: 'www.jolkiroat.fi/blog',
+    likes: 10,
+    id: '5c69a7519cb54625f7aa4247',
+  },
+  {
+    title: 'Jonnen Jälkiruoat',
+    author: 'Jonne Jononen',
+    url: 'www.jolkiroat.fi/blog',
+    likes: 10,
+    id: '5c69b49d0497b72c6d94008d',
+  },
+  {
+    title: 'Jonnen Aaron Music Blog',
+    author: 'Jonne Aaron',
+    url: 'www.aaronmusic.fi/blog',
+    likes: 0,
+    id: '5c6a596a5be1232f2d612c8f',
+  },
+  {
+    title: 'James Bond adventures',
+    author: 'James Bond',
+    url: 'www.jamesbond.com/blog',
+    likes: 16,
+    id: '5c6a5e7bdb729630e50b4a48',
+  },
+];
+
+describe('dummy tests', () => {
+  test('dummy return 1', () => {
+    const result = listHelper.dummy(noblogs);
     expect(result).toBe(1);
   });
 });
 
 describe('totalLikes tests', () => {
   test('Empty array returns 0', () => {
-    const blogs = [];
-
-    const result = listHelper.totalLikes(blogs);
+    const result = listHelper.totalLikes(noblogs);
     expect(result).toBe(0);
   });
 
   test('One blog returns 5', () => {
-    const blogs = [
+    const customblogs = [
       {
         title: 'Kesäkurpitsa pizza reseptit',
         author: 'Heli',
@@ -28,12 +64,12 @@ describe('totalLikes tests', () => {
       },
     ];
 
-    const result = listHelper.totalLikes(blogs);
+    const result = listHelper.totalLikes(customblogs);
     expect(result).toBe(5);
   });
 
   test('One blog, no likes parameter, returns 0', () => {
-    const blogs = [
+    const customblogs = [
       {
         title: 'Kesäkurpitsa pizza reseptit',
         author: 'Heli',
@@ -42,55 +78,17 @@ describe('totalLikes tests', () => {
       },
     ];
 
-    const result = listHelper.totalLikes(blogs);
+    const result = listHelper.totalLikes(customblogs);
     expect(result).toBe(0);
   });
 
   test('Many blogs returns 41', () => {
-    const blogs = [
-      {
-        title: 'Kesäkurpitsa pizza reseptit',
-        author: 'Heli',
-        url: 'www.kurpizzab.blog',
-        likes: 5,
-        id: '5c69a6d19cb54625f7aa4246',
-      },
-      {
-        title: 'Jonnen Jälkiruoat',
-        author: 'Jonne Jononen',
-        url: 'www.jolkiroat.fi/blog',
-        likes: 10,
-        id: '5c69a7519cb54625f7aa4247',
-      },
-      {
-        title: 'Jonnen Jälkiruoat',
-        author: 'Jonne Jononen',
-        url: 'www.jolkiroat.fi/blog',
-        likes: 10,
-        id: '5c69b49d0497b72c6d94008d',
-      },
-      {
-        title: 'Jonnen Aaron Music Blog',
-        author: 'Jonne Aaron',
-        url: 'www.aaronmusic.fi/blog',
-        likes: 0,
-        id: '5c6a596a5be1232f2d612c8f',
-      },
-      {
-        title: 'James Bond adventures',
-        author: 'James Bond',
-        url: 'www.jamesbond.com/blog',
-        likes: 16,
-        id: '5c6a5e7bdb729630e50b4a48',
-      },
-    ];
-
     const result = listHelper.totalLikes(blogs);
     expect(result).toBe(41);
   });
 
   test('Many blogs returns 0', () => {
-    const blogs = [
+    const noLikesBlogs = [
       {
         title: 'Kesäkurpitsa pizza reseptit',
         author: 'Heli',
@@ -128,51 +126,13 @@ describe('totalLikes tests', () => {
       },
     ];
 
-    const result = listHelper.totalLikes(blogs);
+    const result = listHelper.totalLikes(noLikesBlogs);
     expect(result).toBe(0);
   });
 });
 
 describe('favouriteBlog tests', () => {
-
   test('Blog with most likes', () => {
-    const blogs = [
-      {
-        title: 'Kesäkurpitsa pizza reseptit',
-        author: 'Heli',
-        url: 'www.kurpizzab.blog',
-        likes: 5,
-        id: '5c69a6d19cb54625f7aa4246',
-      },
-      {
-        title: 'Jonnen Jälkiruoat',
-        author: 'Jonne Jononen',
-        url: 'www.jolkiroat.fi/blog',
-        likes: 10,
-        id: '5c69a7519cb54625f7aa4247',
-      },
-      {
-        title: 'Jonnen Jälkiruoat',
-        author: 'Jonne Jononen',
-        url: 'www.jolkiroat.fi/blog',
-        likes: 10,
-        id: '5c69b49d0497b72c6d94008d',
-      },
-      {
-        title: 'Jonnen Aaron Music Blog',
-        author: 'Jonne Aaron',
-        url: 'www.aaronmusic.fi/blog',
-        likes: 0,
-        id: '5c6a596a5be1232f2d612c8f',
-      },
-      {
-        title: 'James Bond adventures',
-        author: 'James Bond',
-        url: 'www.jamesbond.com/blog',
-        likes: 16,
-        id: '5c6a5e7bdb729630e50b4a48',
-      },
-    ];
     const expectedResult = {
       title: 'James Bond adventures',
       author: 'James Bond',
@@ -186,14 +146,51 @@ describe('favouriteBlog tests', () => {
   });
 
   test('Blogs empty results in empty objects', () => {
-    const blogs = [];
-    const resultObject = listHelper.favouriteBlog(blogs);
+    const resultObject = listHelper.favouriteBlog(noblogs);
 
     expect(resultObject).toEqual({});
   });
 
   test('Blogs not array results in empty object', () => {
     const resultObject = listHelper.favouriteBlog('NotAnArray');
+
+    expect(resultObject).toEqual({});
+  });
+});
+
+describe('mostBlogs tests', () => {
+  test('Jonne Jononen has most blogs', () => {
+    const resultObject = listHelper.mostBlogs(blogs);
+
+    const sampleResult = {
+      author: 'Jonne Jononen',
+      blogs: 2,
+    };
+
+    expect(resultObject).toEqual(sampleResult);
+  });
+
+  test('mostBlogs empty array', () => {
+    const resultObject = listHelper.mostBlogs(noblogs);
+
+    expect(resultObject).toEqual({});
+  });
+});
+
+describe('mostLikes tests', () => {
+  test('Jonne Jononen has most likes', () => {
+    const resultObject = listHelper.mostLikes(blogs);
+
+    const sampleResult = {
+      author: 'Jonne Jononen',
+      likes: 20,
+    };
+
+    expect(resultObject).toEqual(sampleResult);
+  });
+
+  test('mostLikes empty array', () => {
+    const resultObject = listHelper.mostLikes(noblogs);
 
     expect(resultObject).toEqual({});
   });
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -12,8 +12,52 @@ const favouriteBlog = (blogs) => {
   return blogs[0];
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return {};
+
+  const reducer = (res, blog) => {
+    if (!res[blog.author]) {
+      res[blog.author] = {
+        author: blog.author,
+        blogs: 1,
+      };
+    } else {
+      res[blog.author].blogs += 1;
+    }
+    return res;
+  };
+
+  const resultArray = blogs.reduce(reducer, {});
+  const keysLargestSmallest = Object.keys(resultArray).sort((a, b) => {
+    return resultArray[b].blogs - resultArray[a].blogs;
+  });
+  return resultArray[keysLargestSmallest[0]];
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return {};
+
+  const reducer = (res, blog) => {
+    if (!res[blog.author]) {
+      res[blog.author] = {
+        author: blog.author,
+        likes: blog.likes,
+      };
+    } else {
+      res[blog.author].likes += blog.likes;
+    }
+    return res;
+  };
+  const resultArray = blogs.reduce(reducer, {});
+  const keysLargestSmallest = Object.keys(resultArray).sort((a, b) => resultArray[b].likes - resultArray[a].likes);
+  const keyWithMostLikes = keysLargestSmallest[0];
+  return resultArray[keyWithMostLikes];
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favouriteBlog,
+  mostBlogs,
+  mostLikes,
 };
