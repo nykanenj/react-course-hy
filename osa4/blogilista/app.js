474b32b75aebb52e2d7620d56db770fd1ed3f161
Juuso Nykänen <juusonykanen@hotmail.com>
2019-02-18 10:59:12 -0800

    4.20 done

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -15,6 +15,7 @@ const loginRouter = require('./controllers/login');
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(middleware.tokenExtractor);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -4,14 +4,6 @@ const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get('/info', (request, response) => {
   response.send('Welcome to blogilista API');
 });
@@ -36,10 +28,9 @@ blogsRouter.post('/', async (request, response, next) => {
   } = request.body;
 
   try {
-    const token = getTokenFrom(request);
-    const decodedToken = jwt.verify(token, process.env.SECRET);
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
 
-    if (!token || !decodedToken.id) {
+    if (!request.token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or not valid'});
     }
 
