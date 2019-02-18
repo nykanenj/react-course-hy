5cf67000b0b2ad88c20d32c1e70d7c655540a0b7
Juuso Nykänen <juusonykanen@hotmail.com>
2019-02-18 01:46:01 -0800

    4.5 done

--- osa4/blogilista/tests/listhelper.test.js
+++ osa4/blogilista/tests/listhelper.test.js
@@ -132,3 +132,69 @@ describe('totalLikes tests', () => {
     expect(result).toBe(0);
   });
 });
+
+describe('favouriteBlog tests', () => {
+
+  test('Blog with most likes', () => {
+    const blogs = [
+      {
+        title: 'Kesäkurpitsa pizza reseptit',
+        author: 'Heli',
+        url: 'www.kurpizzab.blog',
+        likes: 5,
+        id: '5c69a6d19cb54625f7aa4246',
+      },
+      {
+        title: 'Jonnen Jälkiruoat',
+        author: 'Jonne Jononen',
+        url: 'www.jolkiroat.fi/blog',
+        likes: 10,
+        id: '5c69a7519cb54625f7aa4247',
+      },
+      {
+        title: 'Jonnen Jälkiruoat',
+        author: 'Jonne Jononen',
+        url: 'www.jolkiroat.fi/blog',
+        likes: 10,
+        id: '5c69b49d0497b72c6d94008d',
+      },
+      {
+        title: 'Jonnen Aaron Music Blog',
+        author: 'Jonne Aaron',
+        url: 'www.aaronmusic.fi/blog',
+        likes: 0,
+        id: '5c6a596a5be1232f2d612c8f',
+      },
+      {
+        title: 'James Bond adventures',
+        author: 'James Bond',
+        url: 'www.jamesbond.com/blog',
+        likes: 16,
+        id: '5c6a5e7bdb729630e50b4a48',
+      },
+    ];
+    const expectedResult = {
+      title: 'James Bond adventures',
+      author: 'James Bond',
+      url: 'www.jamesbond.com/blog',
+      likes: 16,
+      id: '5c6a5e7bdb729630e50b4a48',
+    };
+    const resultObject = listHelper.favouriteBlog(blogs);
+
+    expect(resultObject).toEqual(expectedResult);
+  });
+
+  test('Blogs empty results in empty objects', () => {
+    const blogs = [];
+    const resultObject = listHelper.favouriteBlog(blogs);
+
+    expect(resultObject).toEqual({});
+  });
+
+  test('Blogs not array results in empty object', () => {
+    const resultObject = listHelper.favouriteBlog('NotAnArray');
+
+    expect(resultObject).toEqual({});
+  });
+});
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return likesArr.reduce(reducer, 0);
 };
 
+const favouriteBlog = (blogs) => {
+  if (!Array.isArray(blogs) || blogs.length === 0) return {};
+  blogs.sort((a, b) => a.likes < b.likes);
+  return blogs[0];
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
